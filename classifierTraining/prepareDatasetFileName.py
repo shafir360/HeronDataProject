0cd29080ecf8c0298bf4c53065b0747da492b7f0
shafir360 <shafir360@gmail.com>
2024-11-15 23:44:01 +0000

    semantic trainer and predictor made

--- classifierTraining/prepareDatasetFileName.py
+++ classifierTraining/prepareDatasetFileName.py
@@ -7,8 +7,20 @@ import glob
 #Prepare synthetic filename dataset
 
 
-parent_folder = r"generators\filenameGen\genResult"
+parent_folder_synth = r"generators\filenameGen\genResult"
+parent_folder_real = r"dataset\raw"
 outFolder = 'fileNameDataset'
+
+#helper function
+def find_filename_by_type(folder_path, file_type='.jpg'):
+    filenames = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(file_type):
+                filenames.append(file)  # Append only the filename
+    return filenames
+
+
 
 
 # Get the directory of the current script
@@ -16,7 +28,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 outFolder = os.path.join(script_dir, outFolder)
 
-classes = os.listdir(parent_folder)
+classes_synth = os.listdir(parent_folder_synth)
 
 # Check if the folder exists
 if os.path.exists(outFolder):
@@ -28,11 +40,11 @@ if os.path.exists(outFolder):
 os.makedirs(outFolder)
 
 
-#extract all filenames 
+#extract all filenames from synth dataset
 data_arrays =[]
-for class_ in classes:
+for class_ in classes_synth:
     all_lines = []
-    folder_path = os.path.join(parent_folder, class_)
+    folder_path = os.path.join(parent_folder_synth, class_)
     for file_path in glob.glob(os.path.join(folder_path, "*.txt")):
         with open(file_path, "r") as file:
             # Read all lines from the current file and extend the list
@@ -44,11 +56,10 @@ for class_ in classes:
 
 
 
-
 # Flatten data and labels using list comprehensions
 df = pd.DataFrame({
     "Data": [item for array in data_arrays for item in array],
-    "Label": [label for label, array in zip(classes, data_arrays) for _ in array]
+    "Label": [label for label, array in zip(classes_synth, data_arrays) for _ in array]
 })
 
 
@@ -56,7 +67,7 @@ df = pd.DataFrame({
 
 
 output_path = os.path.join(outFolder,'syntheticFilename')
-
+print(df.shape[0])
 df.to_csv(output_path, index=False)
 
 
